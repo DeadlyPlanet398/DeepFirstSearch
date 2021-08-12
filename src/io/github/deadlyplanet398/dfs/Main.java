0d2b5308a8f5ad7c966b64bf3dfcb2228fe72ff9
DeadlyPlanet398 <mystery-chen@outlook.com>
2021-08-12 19:40:02 +0800

    hw

---
+++ src/io/github/deadlyplanet398/dfs/Main.java
@@ -0,0 +1,12 @@
+package io.github.deadlyplanet398.dfs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        var input=new Scanner(System.in);
+        input.next();
+    }
+}
